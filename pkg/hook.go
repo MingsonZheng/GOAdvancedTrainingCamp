0e2529a4022f9f61c5f2228bb203325ff7e43f1b
mingsonzheng <mingsonzheng@outlook.com>
2022-08-08 11:32:16 +0800

    HTTP Server -- 注册 Hook

--- pkg/hook.go
+++ pkg/hook.go
@@ -19,27 +19,27 @@ func BuildCloseServerHook(servers ...Server) Hook {
 	return func(ctx context.Context) error {
 		wg := sync.WaitGroup{}
 		doneCh := make(chan struct{})
-		wg.Add(len(servers))
+		wg.Add(len(servers)) // 添加 server 的数量，因为我想一个个关闭完
 
-		for _, s := range servers {
-			go func(svr Server) {
+		for _, s := range servers { // 遍历 server
+			go func(svr Server) { // 开 goroutine 每一个 server 单独关闭，为了不影响其他 server
 				err := svr.Shutdown(ctx)
 				if err != nil {
 					fmt.Printf("server shutdown error: %v \n", err)
 				}
 				time.Sleep(time.Second)
-				wg.Done()
+				wg.Done() // 这里就是减一
 			}(s)
 		}
 		go func() {
-			wg.Wait()
+			wg.Wait() // server 没有关完就会卡在这里，关完了就会继续往下执行
 			doneCh <- struct{}{}
 		}()
 		select {
-		case <- ctx.Done():
+		case <-ctx.Done(): // 执行的时候没关完
 			fmt.Printf("closing servers timeout \n")
 			return ErrorHookTimeout
-		case <- doneCh:
+		case <-doneCh: // 执行的时候关完了
 			fmt.Printf("close all servers \n")
 			return nil
 		}
